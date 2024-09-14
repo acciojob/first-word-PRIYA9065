e28a78735f5dea6030e9711de1414fad74bdc323
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-14 18:55:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function firstWord(s) {
   // your code here
-	String str = '';
-
-	for(let i = 0; i < s.length; i++){
-		str = str + s.charAt(i);
-		if(s.charAt(i) == ' '){
-			return str;
-		}
-	}}
+	let ArrOfStr = s.split(" ");
+	let firstWord = ArrOfStr[0];
+	return firstWord;
+}
 // Do not change the code below
 
 const s = prompt("Enter String:");
