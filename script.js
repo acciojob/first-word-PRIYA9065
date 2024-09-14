b92af022f6cad892123ec2e6e09a196911b00796
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-14 18:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function firstWord(s) {
   // your code here
-}
+	String str = '';
 
+	for(let i = 0; i < s.length; i++){
+		str = str + s.charAt(i);
+		if(s.charAt(i) == ' '){
+			return str;
+		}
+	}}
 // Do not change the code below
 
 const s = prompt("Enter String:");
